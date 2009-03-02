9f9fd2a6377c8dcf6cef765165a3385cd5a7912e
cash <cash.costello@gmail.com>
2009-03-02 22:19:11 +0000

    updated for Elgg 1.5

--- views/default/account/forms/forgotten_password.php
+++ views/default/account/forms/forgotten_password.php
@@ -4,6 +4,6 @@
   $form_body .= "<p><b>". elgg_echo('email') . "</b> " . elgg_view('input/text', array('internalname' => 'email')) . "</p>";
   $form_body .= "<p>" . elgg_view('input/submit', array('value' => elgg_echo('request'))) . "</p>";
 ?>
-<div id="forgotten_box">
-  <?php echo elgg_view('input/form', array('action' => "{$vars['url']}actions/user/requestnewpassword", 'body' => $form_body)); ?>
+<div class="contentWrapper">
+  <?php echo elgg_view('input/form', array('action' => "{$vars['url']}action/user/requestnewpassword", 'body' => $form_body)); ?>
 </div>
